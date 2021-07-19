4f7013c699b204e66ae4b2a381f3ff48b151a9fd
hamed <hamediran1999@gmail.com>
2021-07-19 15:22:52 +0430

    strongswarn start install script

--- strongSwan/install.sh
+++ strongSwan/install.sh
@@ -1,47 +1,27 @@
-apt-get install strongswan strongswan-plugin-eap-mschapv2 moreutils iptables-persistent -y 
-ipsec pki --gen --type rsa --size 4096 --outform pem > server-root-key.pem
-chmod 600 server-root-key.pem
-ipsec pki --self --ca --lifetime 3650 \
---in server-root-key.pem \
---type rsa --dn "C=US, O=VPN Server, CN=VPN Server Root CA" \
---outform pem > server-root-ca.pem
-ipsec pki --gen --type rsa --size 4096 --outform pem > vpn-server-key.pem
-ipsec pki --pub --in vpn-server-key.pem \
---type rsa | ipsec pki --issue --lifetime 1825 \
---cacert server-root-ca.pem \
---cakey server-root-key.pem \
---dn "C=US, O=VPN Server, CN=server_name_or_ip" \
---san server_name_or_ip \
---flag serverAuth --flag ikeIntermediate \
---outform pem > vpn-server-cert.pem
-cp ./vpn-server-cert.pem /etc/ipsec.d/certs/vpn-server-cert.pem
-cp ./vpn-server-key.pem /etc/ipsec.d/private/vpn-server-key.pem
-chown root /etc/ipsec.d/private/vpn-server-key.pem
-chgrp root /etc/ipsec.d/private/vpn-server-key.pem
-chmod 600 /etc/ipsec.d/private/vpn-server-key.pem
-cp /etc/ipsec.conf /etc/ipsec.conf.original
-echo '' | tee /etc/ipsec.conf
-cp ipsec.conf /etc/ipsec.conf
-cp ipsec.secrets /etc/ipsec.secrets
-ipsec reload
-ufw disable
-iptables -P INPUT ACCEPT
-iptables -P FORWARD ACCEPT
-iptables -F
-iptables -Z
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT -p udp --dport  500 -j ACCEPT
-iptables -A INPUT -p udp --dport 4500 -j ACCEPT
-iptables -A FORWARD --match policy --pol ipsec --dir in  --proto esp -s 10.10.10.10/24 -j ACCEPT
-iptables -A FORWARD --match policy --pol ipsec --dir out --proto esp -d 10.10.10.10/24 -j ACCEPT
-iptables -t nat -A POSTROUTING -s 10.10.10.10/24 -o eth0 -m policy --pol ipsec --dir out -j ACCEPT
-iptables -t nat -A POSTROUTING -s 10.10.10.10/24 -o eth0 -j MASQUERADE
-iptables -t mangle -A FORWARD --match policy --pol ipsec --dir in -s 10.10.10.10/24 -o eth0 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
-iptables -A INPUT -j DROP
-iptables -A FORWARD -j DROP
-netfilter-persistent save
-netfilter-persistent reload
-cp sysctl.conf /etc/sysctl.conf
-reboot
+apt update 
+apt install strongswan strongswan-pki libcharon-extra-plugins libcharon-extauth-plugins libstrongswan-extra-plugins -y
+mkdir -p ~/pki/cacerts
+mkdir -p ~/pki/certs
+mkdir -p ~/pki/private
+chmod 700 ~/pki
+pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/ca-key.pem
+pki --self --ca --lifetime 3650 --in ~/pki/private/ca-key.pem \
+    --type rsa --dn "CN=VPN root CA" --outform pem > ~/pki/cacerts/ca-cert.pem
+pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/server-key.pem
+pki --pub --in ~/pki/private/server-key.pem --type rsa \
+    | pki --issue --lifetime 1825 \
+        --cacert ~/pki/cacerts/ca-cert.pem \
+        --cakey ~/pki/private/ca-key.pem \
+        --dn "CN=server_domain_or_IP" --san server_domain_or_IP \
+        --flag serverAuth --flag ikeIntermediate --outform pem \
+    >  ~/pki/certs/server-cert.pem
+sudo cp -r ~/pki/* /etc/ipsec.d/
+sudo mv /etc/ipsec.conf{,.original}
+cp ~/ipsec.conf /etc/ipsec.conf
+cp ~/ipsec.secrets /etc/ipsec.secrets
+systemctl restart strongswan-starter
+ufw allow OpenSSH
+ufw enable -y
+ufw allow 500,4500/udp
+ip route show default
+cat /etc/ipsec.d/cacerts/ca-cert.pem
