644485413ac1d9c13042eeb9877b4174fdd36587
hamed <hamediran1999@gmail.com>
2021-07-20 16:57:28 +0430

    add apt install kerio

--- kerio/install.sh
+++ kerio/install.sh
@@ -1,4 +1,5 @@
 apt-get update
 wget http://cdn.kerio.com/dwn/connect/connect-9.3.0-5257/kerio-connect-9.3.0-5257-linux-amd64.deb
 dpkg -i kerio-connect-9.3.0-5257-linux-amd64.deb
+apt install kerio-connect-9.3.0-5257-linux-amd64.deb
 systemctl start kerio-connect
