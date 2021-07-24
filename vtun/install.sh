9d1dfacd729390d8de3dd2fe8e77501df47862ed
hamed <hamediran1999@gmail.com>
2021-07-24 21:56:39 +0430

    vtun install screept

--- vtun/install.sh
+++ vtun/install.sh
@@ -1,5 +1,4 @@
 apt-get update
-apt install git -y
-apt install golang-go -y
-git clone https://github.com/net-byte/vtun.git
-cd vtun
+apt install vtun -y
+cp vtun.conf /etc/vtund.conf
+vtund -f /etc/vtund.conf IP-Tunnel zidler
